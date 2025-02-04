5787924793f342509ca6e83b2dd4e86d83bbb1c7
muhammad-abdullah-khan999 <iamgoodman222@gmail.com>
2025-02-05 00:26:46 +0500

    client.ts changed

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: '2023-01-01',
-  token: process.env.SANITY_API_TOKEN!,
+  token: process.env.SANITY_API_TOKEN,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 });
